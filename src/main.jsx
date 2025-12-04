42be19c04b12207537b11b9c42d06d37657072fd
Rathanak-Phan <studyingalaxy123@gmail.com>
2025-12-04 16:47:21 +0700

    Upadte folder structure and fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+// src/main.jsx  (or main.js)
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' 
+import App from './App'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/FirstWeb_Computer">
-    <App />
-  </BrowserRouter>
-);
+AOS.init({
+  duration: 800,
+  once: true,
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>      
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/FirstWeb_Computer/",
-
   plugins: [react(), tailwindcss()],
 });
