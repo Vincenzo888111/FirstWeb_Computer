f0d0403462378c820c3f85e4c8439ba707ea8ffa
Vincenzo888111 <kornvathanak@gmail.com>
2025-12-04 12:29:07 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  export default {
+  base: '/FirstWeb_Computer/',
+};
+
   plugins: [react(), tailwindcss(),],
 })
